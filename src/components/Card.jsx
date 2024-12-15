f7bc581c762c470a8ffae422fef548d4b3cd8d4e
Shyam-design634 <meghashyamdora.hanumanthu@slu.edu>
2024-12-15 23:07:56 +0000

    commit changes to card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = ({description, alt_description, id, user, urls, likes}) => {
-
-  const style = {
-    backgroundImage: `url(${urls.small})`
-  }
-  
+const Card = ({ id, name, description, price }) => {
   return (
-    <div className="fl w-50 w-25-m w-20-l pa2">
-      <Link to={`/product/${id}`} className="db link dim tc"> 
-        <div style={style} alt="" className="w-100 db outline black-10 h4 cover"></div>
-        <dl className="mt2 f6 lh-copy">
-          <dt className="clip">Title</dt>
-          <dd className="ml0 black truncate w-100">{description ?? alt_description}</dd>
-          <dt className="clip">Artist</dt>
-          <dd className="ml0 gray truncate w-100">{user.first_name} {user.last_name}</dd>
-          <dt className="clip">Likes</dt>
-          <dd className="ml0 gray truncate w-100">{likes} Likes</dd>
-        </dl>
+    <div className="ba b--black-20 pa3 ma2">
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>Price: ${price}</p>
+      {/* Link to the SingleView for the specific product */}
+      <Link to={`/product/${id}`} className="blue underline pointer">
+        View Details
       </Link>
     </div>
-  )
-}
+  );
+};
+
 
 export default Card;
