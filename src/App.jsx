d7e407b6684ced619b34745e710e6d9092e37d5f
Shyam-design634 <meghashyamdora.hanumanthu@slu.edu>
2024-12-15 21:53:13 +0000

    Added cardlist into the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@
 import Header from './components/Header';
 
 import productData from './data/full-products';
+// App.jsx
+
+// ...
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CardList />
     </div>
   );
 }
